7e3dcb56e54ed5d78605c7f907a5c05408b212d3
Talha Khalil <talhakhalil535@gmail.com>
2026-06-16 13:13:57 +0500

    update pgboss config to delete archived jobs

--- src/queue/boss.js
+++ src/queue/boss.js
@@ -7,7 +7,19 @@ export const boss = new PgBoss({
   connectionString: config.databaseUrl,
   retryLimit: 3,
   retryDelay: 5,
-  deleteAfterDays: 7,
+
+  // Move completed jobs to archive after 1 hour
+  archiveCompletedAfterSeconds: 60 * 60,
+
+  // Move failed jobs to archive after 1 hour
+  archiveFailedAfterSeconds: 60 * 60,
+
+  // Delete from archive after 1 hour (3600 seconds)
+  deleteArchivedAfterSeconds: 60 * 60,
+
+  // Run cleanup every 5 minutes
+  maintenanceIntervalSeconds: 300,
+
   ssl: {
     rejectUnauthorized: false,
   },
